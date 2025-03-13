a6056915f1ab3d4f86bb75886513dfa970c710c9
Luis Amezcua <laamezcu@usc.edu>
2025-03-13 11:47:43 -0700

    first commit

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,25 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool pathCheck(Node* root, int height, int& leafHeight) {
+    if (!root) return true;
+
+    if (!root->left && !root->right) {
+        if (leafHeight == -1) {
+            leafHeight = height;
+        }
+        return height == leafHeight;
+    }
+
+    return pathCheck(root->left, height + 1, leafHeight) &&
+            pathCheck(root->right, height + 1, leafHeight);
+}
+
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    int leafHeight = -1;
+    return pathCheck(root, 0, leafHeight);
 }
 
